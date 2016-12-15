e891441e2e10fdead966388f50b5bbfdda548704
timcartwright <tim@websitesbytim.com>
2016-12-15 10:39:00 +0000

    Split layout with sidebar

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 import App from 'grommet/components/App';
+import Sidebar from 'grommet/components/Sidebar';
+import Split from 'grommet/components/Split';
 import Box from 'grommet/components/Box';
-// import Anchor from 'grommet/components/Anchor';
+import Menu from 'grommet/components/Menu';
+import Anchor from 'grommet/components/Anchor';
 import Header from 'grommet/components/Header';
 import Footer from 'grommet/components/Footer';
 import Title from 'grommet/components/Title';
+import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
+import FormFields from 'grommet/components/FormFields';
+import FormField from 'grommet/components/FormField';
+import Form from 'grommet/components/Form';
+import RadioButton from 'grommet/components/RadioButton';
 import Select from 'grommet/components/Select';
 
 import BusMap from './components/BusMap';
@@ -37,31 +46,63 @@ class Main extends Component {
         if (this.props.lines === []) return <div>Waiting for data</div>;
         return (
             <App centered={false}>
-                <Box full={true}>
-                    <Header direction='row'
-                        justify='between'
-                        pad={{horizontal: 'medium'}} >
-                        <Title>
-                          Busmap
-                        </Title>
-                        <Select placeHolder='Search'
-                                inline={false}
-                                options={this.props.lines.map(line => line.id)}
-                                value={this.props.config.line}
-                                onChange={this.selectHandler.bind(this)} />
-                    </Header>
-                    <BusMap />
-                    <Footer primary={true}
-                            appCentered={true} 
-                            direction='column'
-                            align='center'
-                            pad='small'
-                            colorIndex='grey-1' >
-                        <p>
-                             Made by TeaSea
-                        </p>
-                    </Footer>
-                </Box>
+                <Split fixed={false}
+                    flex="right">
+                    <Sidebar colorIndex="neutral-1" size="small">
+                        <Header pad="medium" direction="column" align="start">
+                            <Title>
+                                BusMapper
+                            </Title>
+                            <Heading tag="h6">
+                                Live London Bus Map
+                            </Heading>
+                        </Header>
+                        <Box flex="grow"
+                            justify="start">
+                            <Form pad="small">
+                                
+                                <Heading tag="h2">
+                                    Bus Number
+                                </Heading>
+                                <FormFields>
+                                    <Select placeHolder='Search'
+                                            inline={false}
+                                            options={this.props.lines.map(line => line.id)}
+                                            value={this.props.config.line}
+                                            onChange={this.selectHandler.bind(this)} />
+                                    <Heading tag="h2">
+                                        Direction
+                                    </Heading>  
+                                    <FormField>
+                                        <RadioButton id="inbound"
+                                            name="inbound"
+                                            label="Inbound"
+                                            checked={true} />
+                                        <RadioButton id="outbound"
+                                            name="outbound"
+                                            label="Outbound"
+                                            checked={false} />
+                                    </FormField>
+                                </FormFields>
+                            </Form>
+                        </Box>
+                        <Footer pad="medium" direction="column" align="start">
+                            <Paragraph size="medium">
+                                BusMapper is a live bus map that uses data from the TFL API to estimate the location and movement of London buses.
+                            </Paragraph>
+                            <Anchor href="//teasea.uk"
+                                className="active">
+                                Made by TeaSea
+                            </Anchor>
+                                
+                        </Footer>
+                    </Sidebar>
+                    <Box colorIndex="neutral-2"
+                        justify="center"
+                        align="center">
+                        <BusMap />
+                    </Box>
+                </Split>
           </App>
         );
     }
